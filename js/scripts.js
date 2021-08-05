f948ff6c4b2a985ed8ab434936e9ff57b6937023
Erin Egbert <erinegbert6@gmail.com>
2021-08-05 14:04:51 -0700

    adding a way to cross out list items

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,10 @@ function newItem () {
     $('#list').append(li);
   }
 
+  //2. Crossing out an item from the list
+  function crossOut() {
+    li.toggleClass('strike');
+  }
+
+  li.on('dblclick', crossOut);
 }
